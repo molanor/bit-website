1cc6f1582a26becccfed757a27ac2148f6edb1ae
Juan Pablo Molano Rojas <molanor.juan@gmail.com>
2025-04-24 21:43:21 -0500

    Descripción de los cambios realizados

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const apellidos = "Molano Rojas";
 const edad = 35;
 
 console.log(`Soy ${nombreCompleto} ${apellidos} y tengo ${edad} años`);
+
+/*
+- Cree dos variables de tipo constante y asigne los valores 20 y 3 respectivamente
+- Cree una constante para guardar el resultado de cada una de las operaciones básicas (suma, resta, multiplicación y división)
+- Imprima el resultado de cada operación con el siguiente formato de ejemplo:
+20 + 3 = 23
+*/
+//clearconstantes*/
+const num1 = 20;
+const num2 = 3;
+
+// Operaciones*/
+//const suma = num1 + num2;
+//const resta = num1 - num2;
+//const multiplicacion = num1 * num2;
+//const division = num1 / num2;
+
+// Impresión de resultados*/
+//console.log(`${num1} + ${num2} = ${suma}`);
+//console.log(`${num1} - ${num2} = ${resta}`);
+//console.log(`${num1} * ${num2} = ${multiplicacion}`);
+//console.log(`${num1} / ${num2} = ${division}`);
+
+const num1 = 12
